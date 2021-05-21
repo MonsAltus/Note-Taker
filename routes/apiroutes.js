b4c7a1403bc96555740112b895d57d80edec95fd
James <James.Hacklander@gmail.com>
2021-05-21 16:57:06 -0700

    Developed apiRoutes. Made style sheets work on notes page.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,5 +6,9 @@ const db = require('../db/db.json')
 
 module.exports = (app) => {
     app.get('/api/notes', (req,res) => res.json(db))
-    
+
+    app.post('notes', (req,res) => {
+        
+    })
+
 }
