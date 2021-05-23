6e9cdc0367b4c5237898dcc3f861fa32521060be
James <James.Hacklander@gmail.com>
2021-05-23 09:40:13 -0700

    Attempting to fix error for heroku deployment.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = (app) => {
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
     app.get('/', (req,res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
     })
