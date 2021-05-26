becd9d81a15a4c80137942a9239f745889e41445
James <James.Hacklander@gmail.com>
2021-05-26 11:12:56 -0700

    Fixed API routing error.

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static('public'))
 
-require('./routes/htmlRoutes.js')(app)
 require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
 
 app.listen(PORT, () =>  {
     console.log(`Listening on PORT: ${PORT}`)
